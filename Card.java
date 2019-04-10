6403ada3ef9fb9fa85adee2954f7f4d2477c2cdc
melaniefox <mefox@seas.upenn.edu>
2019-04-10 08:28:29 -0700

    created a simple card - copied from eclipse

---
+++ Card.java
@@ -0,0 +1,52 @@
+
+/**
+ * this class builds a Card object
+ * these are basically blank cards that have no rant, suit, or points
+ * all of those are added by the Deck class
+ * @author melaniefox
+ *
+ */
+public class Card {
+	
+	//instance variables
+	private String rank;
+	private String suit;
+	private int points;
+	
+	/**
+	 * Constructor to build a card
+	 * takes the rank, suit and points for each card
+	 * @param rnk
+	 * @param st
+	 * @param pts
+	 */
+	Card(String rnk, String st, int pts){
+		this.rank=rnk;
+		this.suit=st;
+		this.points = pts;
+	}
+
+	
+	
+
+	/**
+	 * getters. no setters!
+	 * @return
+	 */
+	public String getRank() {
+		return rank;
+	}
+
+
+	public String getSuit() {
+		return suit;
+	}
+
+
+	public int getPoints() {
+		return points;
+	}
+
+	
+	
+}
